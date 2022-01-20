c4dda982faf3ccca2af15ed9f1721e9d92c4bf52
john-raymon <john@johnraymon.com>
2022-01-20 02:53:01 -0500

    Added controlled select inputs for the product list item's variants(prices)

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,26 +1,98 @@
-import { useEffect } from 'react'
-import { Link, routes } from '@redwoodjs/router'
+import { useEffect, useState } from 'react'
+// import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
+import { Button } from 'lite-react-ui'
 
 const HomePage = () => {
+  const [products, setProducts] = useState([])
+  const [selectedProductPrices, setSelectedProductPrices] = useState({})
   useEffect(() => {
     fetch(`${window.RWJS_API_URL}/fetchProducts`)
       .then((response) => response.json())
-      .then((json) => console.log(json))
+      .then(({ success = false, products = [] }) => {
+        if (success) {
+          const productsModified = products.map((product) => {
+            const prices = product.prices.reduce((acc, price) => {
+              acc[price.id] = price
+              return acc
+            }, {})
+            return { ...product, pricesById: prices }
+          })
+          setProducts(productsModified)
+        }
+      })
       .catch((error) => console.log(error))
   }, [])
+
+  function handleChange(e) {
+    console.log('e.target.name', e.target.name)
+    console.log('e.target.value', e.target.value)
+    setSelectedProductPrices({
+      ...selectedProductPrices,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    // TODO: Add logic to add item to cart
+  }
+
   return (
     <>
       <MetaTags title="Home" description="Home page" />
-
-      <h1>HomePage</h1>
-      <p>
-        Find me in <code>./web/src/pages/HomePage/HomePage.js</code>
-      </p>
-      <p>
-        My default route is named <code>home</code>, link to me with `
-        <Link to={routes.home()}>Home</Link>`
-      </p>
+      <ul className="w-full max-w-[45.75rem] mx-auto pb-[5rem] space-y-[6rem]">
+        {products.map(({ product, prices, pricesById }) => {
+          return (
+            <li key={product.id} className="w-full items-center">
+              <div className="flex flex-col justify-center">
+                <img
+                  alt="Long-sleeve white shirt with 202> on it."
+                  className="w-full"
+                  src={product.images[0]}
+                />
+                <h2 className="uppercase text-merch-blue text-center">
+                  {product.name}&nbsp;-&nbsp;
+                  {selectedProductPrices[product.id] ? (
+                    `$${
+                      pricesById[selectedProductPrices[product.id]].unit_amount
+                    }`
+                  ) : (
+                    <span className="opacity-[0.7]">
+                      (Select a size to view price)
+                    </span>
+                  )}
+                </h2>
+                <form
+                  name={product.id}
+                  onChange={handleChange}
+                  onSubmit={handleSubmit}
+                >
+                  <select
+                    name={product.id}
+                    defaultValue={selectedProductPrices[product.id] || ''}
+                    className="flex flex-shrink self-center border-b-2 border-merch-blue my-2 p-2 bg-transparent text-left mx-auto w-auto"
+                  >
+                    <option value="" disabled>
+                      Select Size
+                    </option>
+                    {prices.map(({ id = '', nickname = '' }) => {
+                      return (
+                        <option value={id} key={id}>
+                          {nickname}
+                        </option>
+                      )
+                    })}
+                  </select>
+                  <Button className="mx-auto my-4 !border-[transparent] !bg-merch-blue !text-merch-yellow !uppercase !rounded-[1rem] !text-[1.1rem] !leading-[1.25rem] !py-[1.125rem]">
+                    Add to cart
+                  </Button>
+                </form>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
     </>
   )
 }
