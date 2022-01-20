92ed33c46c956e770d737f4b8b8ef94b6877b5e8
john-raymon <john@johnraymon.com>
2022-01-20 02:51:51 -0500

    Adjusted default layout styles, added brand colors to tailwindcss config

--- web/config/tailwind.config.js
+++ web/config/tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ['src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'merch-blue': '#2D4CEC',
+        'merch-yellow': '#FDD524',
+      },
+    },
   },
   plugins: [],
 }
--- web/src/layouts/DefaultLayout/DefaultLayout.js
+++ web/src/layouts/DefaultLayout/DefaultLayout.js
@@ -4,15 +4,15 @@ import { Button } from 'lite-react-ui'
 const DefaultLayout = ({ children }) => {
   return (
     <>
-      <header className="w-full py-12 px-20">
-        <div className="absolute left-0 right-0">
-          <Logo className="mx-auto" />
+      <header className="top-0 fixed w-full py-[3.125rem] px-3 md:px-20 flex items-center justify-end z-20">
+        <div className="absolute top-0 w-full left-0 right-0 h-full flex items-center">
+          <Logo className="mx-auto w-[20%] md:w-[10rem]" />
         </div>
-        <Button className="float-right !bg-[#FDD524] !text-[#2D4CEC] !uppercase !rounded-[1rem] !text-[1.1rem] !leading-[1.25rem] !py-[1.125rem]">
+        <Button className="float-right !border-[transparent] !bg-[#FDD524] !text-[#2D4CEC] !uppercase !rounded-[1rem] !text-[1.1rem] !leading-[1.25rem] !py-[1.125rem]">
           cart (0)
         </Button>
       </header>
-      <main>{children}</main>
+      <main className="mt-[10rem]">{children}</main>
     </>
   )
 }
